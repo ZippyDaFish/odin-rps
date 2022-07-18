0b3142ee3882d41eed523ef086867faa42fe3253
ZippyDaFish <andrew.riker21@gmail.com>
2022-07-17 20:09:39 -0400

    Create UI branch, prepare script.js for UI input

--- script.js
+++ script.js
@@ -33,22 +33,11 @@ function getComputerChoice() {
 }
 
 function game() {
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Rock, Paper, or Scissors?");
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(playerSelection, computerSelection);
-        console.log(playerScore, computerScore);
-    }
-    if(playerScore === computerScore){
-        console.log("It's a tie.");
-    }
-    else if(playerScore > computerScore){
-        console.log("You win!");
-    }
-    else if(playerScore < computerScore){
-        console.log("You lose!");
-    }
+    const playerSelection = prompt("Rock, Paper, or Scissors?");
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection);
+    console.log(playerScore, computerScore);
 }
 const choices = ['rock', 'paper', 'scissors'];
 var playerScore = 0;
